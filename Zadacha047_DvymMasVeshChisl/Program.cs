5c2078c00709f9870a114be55214bc234f689f15
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-29 22:04:30 +0500

    add Zadacha047_DvymMasVeshChisl

---
+++ Zadacha047_DvymMasVeshChisl/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+double[,] array = new double [m, n];
+FillArray(array);
+PrintArray(array);
+void FillArray(double[,] array)                                          
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+Console.WriteLine($"Полученный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
